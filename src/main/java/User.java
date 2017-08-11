c5cf13e96e908ba902de70405fb70ca1f83cf796
s3330467 <s3330467@student.rmit.edu.au>
2017-08-11 10:48:17 +1000

    fixed untracked files

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -37,7 +37,7 @@ public class User {
                 ", lng=" + lng +
                 '}';
     }
-
+    //test
     public static void updateUserList() {
         userList = DB.fetchUsersFromDB();
     }
