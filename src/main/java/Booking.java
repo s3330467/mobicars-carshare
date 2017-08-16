6cd0db245fe2faa93947ba552b5c469428c074e9
Alextg1 <s3330467@student.rmit.edu.au>
2017-08-16 19:16:25 +1000

    fixed merge

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -1,15 +1,9 @@
 import java.util.*;
 
 public class Booking {
-<<<<<<< HEAD
-    private String booking_id;
+    private String id;
     private String user_id;
     private String car_id;
-=======
-    private int id;
-    private int user_id;
-    private int car_id;
->>>>>>> sprint2-alex
     private String start_date;
     private String end_date;
     private String start_time;
@@ -21,39 +15,14 @@ public class Booking {
     private double end_lng;
     public static List<Booking> bookingList = new ArrayList<Booking>();
     
-<<<<<<< HEAD
         public Booking(String booking_id, String user_id, String car_id, String start_date, String start_time, double start_lat, double start_lng) {
-            this.booking_id = booking_id;
-=======
-        public Booking(int booking_id, int user_id, int car_id, String start_date, String start_time, double start_lat, double start_lng) {
             this.id = booking_id;
->>>>>>> sprint2-alex
             this.user_id = user_id;
             this.car_id = car_id;
             this.start_date = start_date;
             this.start_time = start_time;
             this.start_lat = start_lat;
             this.start_lng = start_lng;
-        }
-    
-<<<<<<< HEAD
-    public Booking(String booking_id, String user_id, String car_id, String start_date, String end_date, String start_time, String end_time, double cost, double start_lat, double start_lng, double end_lat, double end_lng) {
-            this.booking_id = booking_id;
-=======
-    public Booking(int booking_id, int user_id, int car_id, String start_date, String end_date, String start_time, String end_time, double cost, double start_lat, double start_lng, double end_lat, double end_lng) {
-            this.id = booking_id;
->>>>>>> sprint2-alex
-            this.user_id = user_id;
-            this.car_id = car_id;
-            this.start_date = start_date;
-            this.end_date = end_date;
-            this.start_time = start_time;
-            this.end_time = end_time;
-            this.cost = cost;
-            this.start_lat = start_lat;
-            this.start_lng = start_lng;
-            this.end_lat = end_lat;
-            this.end_lng = end_lng;
         }
 
     @Override
@@ -61,114 +30,108 @@ public class Booking {
         return "Booking{" + "booking_id=" + id + ", user_id=" + user_id + ", car_id=" + car_id + ", start_date=" + start_date + ", end_date=" + end_date + ", start_time=" + start_time + ", end_time=" + end_time + ", cost=" + cost + ", start_lat=" + start_lat + ", start_lng=" + start_lng + ", end_lat=" + end_lat + ", end_lng=" + end_lng + '}';
     }
     
-            public static void updateBookingList() {
+    public static void updateBookingList() {
         bookingList = DB.fetchBookings();
-            }
-        
-<<<<<<< HEAD
-        public String getBooking_id() {
-        return booking_id;
-        }
-        
-        public void setBooking_id(String booking_id) {
-        this.booking_id = booking_id;
-=======
-        public int getBooking_id() {
+    }
+
+    public String getId() {
         return id;
-        }
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getCar_id() {
+        return car_id;
+    }
+
+    public void setCar_id(String car_id) {
+        this.car_id = car_id;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public double getStart_lat() {
+        return start_lat;
+    }
+
+    public void setStart_lat(double start_lat) {
+        this.start_lat = start_lat;
+    }
+
+    public double getStart_lng() {
+        return start_lng;
+    }
+
+    public void setStart_lng(double start_lng) {
+        this.start_lng = start_lng;
+    }
+
+    public double getEnd_lat() {
+        return end_lat;
+    }
+
+    public void setEnd_lat(double end_lat) {
+        this.end_lat = end_lat;
+    }
+
+    public double getEnd_lng() {
+        return end_lng;
+    }
+
+    public void setEnd_lng(double end_lng) {
+        this.end_lng = end_lng;
+    }
+    
+    
         
-        public void setBooking_id(int booking_id) {
-        this.id = booking_id;
->>>>>>> sprint2-alex
-        }
-
-        public String getUser_id() {
-            return user_id;
-        }
-
-        public void setUser_id(String user_id) {
-            this.user_id = user_id;
-        }
-
-        public String getCar_id() {
-            return car_id;
-        }
-
-        public void setCar_id(String car_id) {
-            this.car_id = car_id;
-        }
-
-        public String getStart_date() {
-            return start_date;
-        }
-
-        public void setStart_date(String start_date) {
-            this.start_date = start_date;
-        }
-
-        public String getEnd_date() {
-            return end_date;
-        }
-
-        public void setEnd_date(String end_date) {
-            this.end_date = end_date;
-        }
-
-        public String getStart_time() {
-            return start_time;
-        }
-
-        public void setStart_time(String start_time) {
-            this.start_time = start_time;
-        }
-
-        public String getEnd_time() {
-            return end_time;
-        }
-
-        public void setEnd_time(String end_time) {
-            this.end_time = end_time;
-        }
-
-        public double getCost() {
-            return cost;
-        }
-
-        public void setCost(double cost) {
-            this.cost = cost;
-        }
-
-        public double getStart_lat() {
-            return start_lat;
-        }
-
-        public void setStart_lat(double start_lat) {
-            this.start_lat = start_lat;
-        }
-
-        public double getStart_lng() {
-            return start_lng;
-        }
-
-        public void setStart_lng(double start_lng) {
-            this.start_lng = start_lng;
-        }
-
-        public double getEnd_lat() {
-            return end_lat;
-        }
-
-        public void setEnd_lat(double end_lat) {
-            this.end_lat = end_lat;
-        }
-
-        public double getEnd_lng() {
-            return end_lng;
-        }
-
-        public void setEnd_lng(double end_lng) {
-            this.end_lng = end_lng;
-        }
     
     
     }
--- src/main/java/BookingService.java
+++ src/main/java/BookingService.java
@@ -1,11 +1,11 @@
 import org.sql2o.*;
 import java.util.*;
-<<<<<<< HEAD
+import java.text.*;
 
 public class BookingService {
-    public static String sqlDB = "jdbc:mysql://localhost:3306/mobicars";
-    public static String sqlUser = "ubuntu";
-    public static String sqlPass = "password";
+    
+    private static final DateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+    private static final DateFormat time = new SimpleDateFormat("HH:mm:ss");
     
     // returns a single booking by id
     public static Booking getBooking(String booking_id) {
@@ -13,31 +13,12 @@ public class BookingService {
         int i;
         Booking.updateBookingList();
         for (i = 0; i < Booking.bookingList.size(); i++) {
-            if (Booking.bookingList.get(i).getBooking_id().equals(booking_id)) {
+            if (Booking.bookingList.get(i).getId().equals(booking_id)) {
                 return Booking.bookingList.get(i);
             }
         }
         return null;
     }
-    
-    /*public static Booking getBookingByUserId(String user_id) {
-
-        int i;
-        Booking.updateUserList();
-        for(i = 0; i <Booking.bookingist.size(); i++) {
-            if(Booking.bookingList.get(i).getEmail().equals(email)) {
-                System.out.print("checking email: " + User.userList.get(i).getEmail());
-                return User.userList.get(i);
-            }
-        }
-        return null;
-    }*/
-=======
-import java.text.*;
-
-public class BookingService {
-    private static final DateFormat date = new SimpleDateFormat("yyyy-MM-dd");
-    private static final DateFormat time = new SimpleDateFormat("HH:mm:ss");
     
     public static boolean createBooking(Car car, User user) {
         int user_id = Integer.parseInt(user.getId());
@@ -51,9 +32,9 @@ public class BookingService {
         if(DB.insertBooking(user_id, car_id, start_date, start_time, start_lat, start_lng)) {
             DB.updateCarAvailable(car.getPlate_no(), false);
             car.setAvailable(false);
+            Booking.updateBookingList();
             return true;
         }
             return false;
     }
->>>>>>> sprint2-alex
 }
