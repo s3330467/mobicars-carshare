9188d2ae7e6f92e1bbd348f12da84850c45ab1bb
vishalpradhan <s3530077@student.rmit.edu.au>
2017-08-20 03:41:58 +1000

    search menu implemented
    just a dummy data has been implemented within the search function.
    need to code some javascript to make it functional...

---
+++ src/main/resources/public/search.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+var slider = new Slider('#ex2', {});
