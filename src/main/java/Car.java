e09b7602ad976b879c53605cb9d15bff9aaa0615
vishalpradhan <s3530077@student.rmit.edu.au>
2017-08-24 13:02:50 +1000

    added image field
    image field added

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Car {
+    private String image;
     private String id;
     private String type;
     private String make;
@@ -12,7 +13,8 @@ public class Car {
     private boolean available;
     public static List<Car> carList = new ArrayList<Car>();
 
-    public Car(String id, String type, String make, String model, String plate_no, double hourly_price, double lat, double lng, boolean available) {
+    public Car(String image, String id, String type, String make, String model, String plate_no, double hourly_price, double lat, double lng, boolean available) {
+        this.image = image;
         this.id = id;
         this.type = type;
         this.make = make;
@@ -27,6 +29,7 @@ public class Car {
     @Override
     public String toString() {
         return "Car{" +
+                "image='" + image + '\'' +
                 "id='" + id + '\'' +
                 ", type='" + type + '\'' +
                 ", make='" + make + '\'' +
@@ -47,6 +50,14 @@ public class Car {
 
     public void setType(String type) { this.type = type; }
 
+    public String getImage() {
+        return image;
+    }
+
+    public void setIimage(String image) {
+        this.image = image;
+    }
+    
     public String getId() {
         return id;
     }
