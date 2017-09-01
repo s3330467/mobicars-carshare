3ca531765b4dec273880399d9b75b9db4c1899fb
vishalpradhan <s3530077@student.rmit.edu.au>
2017-09-01 17:58:35 +1000

    booking history
    another thing to work on

--- src/main/java/BookingController.java
+++ src/main/java/BookingController.java
@@ -41,24 +41,24 @@ public class BookingController {
             return new ModelAndView(model, "templates/layout_main.vtl");
         }, new VelocityTemplateEngine());
         
-        post("/process_confirm_booking", (request, response) -> {
-            String plate_no = request.queryParams("plate_no");
-            Car.updateCarList();
-            User.updateUserList();
-            System.out.println("confirm booking plate no: " + plate_no);
-            Car car = CarService.getCarByPlate_no(plate_no);
-            User user = UserService.getUserByEmail(request.session().attribute("session_email"));
-            if(BookingService.createBooking(car, user)) {
-                Booking booking = BookingService.getCurrentBookingByUser_id(user.getId());
-                request.session().attribute("session_booking", booking.getId());
-                response.redirect("/booking_details");
-                return null;
-            }
-            else
-            {
-                return "temporary booking failure page";
-            }
-            });
+//        post("/process_confirm_booking", (request, response) -> {
+//            String plate_no = request.queryParams("plate_no");
+//            Car.updateCarList();
+//            User.updateUserList();
+//            System.out.println("confirm booking plate no: " + plate_no);
+//            Car car = CarService.getCarByPlate_no(plate_no);
+//            User user = UserService.getUserByEmail(request.session().attribute("session_email"));
+//            if(BookingService.createBooking(car, user)) {
+//                Booking booking = BookingService.getCurrentBookingByUser_id(user.getId());
+//                request.session().attribute("session_booking", booking.getId());
+//                response.redirect("/booking_details");
+//                return null;
+//            }
+//            else
+//            {
+//                return "temporary booking failure page";
+//            }
+//            });
         
         post("/process_confirm_booking", (request, response) -> {
             String plate_no = request.queryParams("plate_no");
