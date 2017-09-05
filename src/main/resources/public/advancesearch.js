57d3b7c1a5fede847e79d7190cd00578b2fdb4fb
vishalpradhan <s3530077@student.rmit.edu.au>
2017-09-06 01:45:01 +1000

    dropdown menu
    it does populate the dropdown list but still need to work on it.. the model section stil populates the "make" output.  need some work with that.. also need to fix the duplicate output

--- src/main/resources/public/advancesearch.js
+++ src/main/resources/public/advancesearch.js
@@ -3,56 +3,148 @@
  */
 
 $(document).ready(function(){
-    load_json_function('type');
-    function load_json_data(id, parent_id)
+
+ load_json_data('type');
+
+ function load_json_data(id)
+ {
+  var html_code = '';
+  $.getJSON('/cars', function(cars){
+
+   html_code +='<option value="">Select '+id+'</option>';
+   $.each(cars, function(key, car){
+    if(id === 'type')
     {
-       var html_code = '';
-       $.getJSON('car.json', function(data){
-         html_code += '<option value="">Select '+id+'</option>';
-         $.each(data, function(key, value){
-             if(id =='type')
-             {
-                 if(value.parent_id =='0')
-                 {
-                     html_code += '<option value="'+value.id+'">'+value.name+'</option>';
-                     
-                 }
-             }
-             else
-             {
-                 if(value.parent_id == 'parent_id')
-                 {
-                     html_code += '<option value="'+value.id+'">'+value.name+'</option>';
-                     
-                 }
-             }
-         });
-         $('#'+id).html(html_code);
-       });
+     if(car.type === car.type)
+     {
+      html_code += '<option value="'+car.type+'">'+car.type+'</option>';
+     }
     }
     
-    $(document).on('change', '#make', function(){
-  var type_id = $(this).val();
-  if(type_id !='')
+    else//this is what populates the other two dropdown below the type... 
+    {
+     if(car.type === car.type )
+     {
+      html_code += '<option value="'+car.make+'">'+car.make+'</option>';
+     }
+    }
+//    else // just made this to see because the model section is also populating the make output so basically its not working
+//    {
+//      if (car.make === car.make)
+//      {
+//        html_code += '<option value="'+car.model+'">'+car.model+'</option>';
+//      }
+//    }
+   });
+   $('#'+id).html(html_code);
+  });
+
+ }
+ $(document).on('change', '#type', function(){
+  var car_type = $(this).val();
+ 
+  if(car_type !== '')
   {
-   load_json_data('make', type_id);
+   load_json_data('make');
+    alert('second function');
   }
   else
   {
+//   $('#'+id).html(html_code);
    $('#make').html('<option value="">Select Make</option>');
    $('#model').html('<option value="">Select Model</option>');
   }
  });
- $(document).on('change', '#Make', function(){
-  var make_id = $(this).val();
-  if(make_id !='')
+ $(document).on('change', '#make', function(){
+     
+  var car_make = $(this).val();
+  if(car_make !== '')
   {
-   load_json_data('model', make_id);
+   load_json_data('model');
+   alert('third part');
   }
   else
   {
    $('#model').html('<option value="">Select Model</option>');
   }
-    
-    });
+ });
 });
+
+
+
+
+
+
+
+
+//$(document).ready(function(){
+////    alert('test');
+//    load_json_function('type');
+//    
+//    function load_json_function(id)
+//    {
+////       alert('test');
+//       var html_code = '';
+//       var type_list = [100];
+//       var j;
+//       $.getJSON("/cars", function(cars){
+//         html_code += '<option value="">Select '+id+'</option>';
+//         $.each(cars, function(i, car){
+//             
+//             for (j = 0; j < type_list.length; j++)
+//             {
+//                 
+//                if(type_list[j]=== car.type)
+//                {
+//                    //alert(car.type + ' is already in the list');
+//                    break;
+//                }
+//              if(id ==='type')
+//             {
+//                     //alert(car.type + ' is already not in the list');
+//                    type_list.push(car.type);
+//                    html_code += '<option value="'+car.type+'">'+car.type+'</option>';
+//               
+//             }
+//         }
+////             else
+////             {
+////                 if(value.parent_id === 'parent_id')
+////                 {
+////                     html_code += '<option value="'+value.id+'">'+value.name+'</option>';
+////                     
+////                 }
+////             }
+//        
+//         });
+//         alert(html_code);
+//         $(type).html(html_code);
+//       });
+//   
+//    }
+//    
+////    $(document).on('change', '#type', function(){
+////  var type_id = $(this).val();
+////  if(type_id !=='')
+////  {
+////   load_json_data('make', type_id);
+////  }
+////  else
+////  {
+////   $('#make').html('<option value="">Select Make</option>');
+////   $('#model').html('<option value="">Select Model</option>');
+////  }
+//// });
+//// $(document).on('change', '#make', function(){
+////  var make_id = $(this).val();
+////  if(make_id !=='')
+////  {
+////   load_json_data('model', make_id);
+////  }
+////  else
+////  {
+////   $('#model').html('<option value="">Select Model</option>');
+////  }
+////    
+////    });
+//});
