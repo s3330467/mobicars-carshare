64d991f468ef8000ced7c7dd765a6b6a9de6e9e9
Alextg1 <s3330467@student.rmit.edu.au>
2017-09-17 16:18:17 +1000

    javadoc rewrite of Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 import org.sql2o.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -6,21 +7,27 @@ import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+/**
+ * Date: 7.8.17
+ * <p>
+ * Main java class that instantiates the various spark route controllers<p>
+ *
+ * Updated 24.8.17 by Rachel Tan<p>
+ * Runs the program using SSL with the specified keystore location<p>
+ * 
+ * @author Alexander Young
+ */
 public class Main {
+
     public static void main(String[] args) {
-        
-        /* @author: Rachel
-        Date: 24.8.17
-        Runs the program using SSL with the specified keystore location
-        */
         secure("src/main/resources/public/keystore.jks", "mobicars", null, null);
-        
+
         Car.updateCarList();
         Booking.updateBookingList();
         User.updateUserList();
         new UserController(new UserService());
         new CarController(new CarService());
         new BookingController(new BookingService());
-        
+
     }
 }
