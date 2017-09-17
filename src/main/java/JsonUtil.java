4969f730e4f5656f3c2955e497585a00d4b6198b
Alextg1 <s3330467@student.rmit.edu.au>
2017-09-17 16:14:36 +1000

    javadoc rewrite of JsonUtil.java

--- src/main/java/JsonUtil.java
+++ src/main/java/JsonUtil.java
@@ -1,24 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * This class allows you to make a spark route return in Json format by using
- * a Json response transformer, see CarController /cars route for an example
- * @author alex
- */
 import com.google.gson.Gson;
 import spark.ResponseTransformer;
 
+/**
+ * Date: 4.9.17
+ * <p>
+ * This class allows you to make a spark route return in Json format by using a
+ * Json response transformer, see CarController.java /cars route for an example
+ *
+ * @author Alexander Young
+ */
 public class JsonUtil {
- 
-  public static String toJson(Object object) {
-    return new Gson().toJson(object);
-  }
- 
-  public static ResponseTransformer json() {
-    return JsonUtil::toJson;
-  }
+
+    /**
+     * Author: <b>Alexander Young</b><p>
+     * Date: 4.9.17
+     * <p>
+     * converts a java object to a JSON string<p>
+     *
+     * @param object the object to convert to JSON
+     * @return a stringified version of the object in JSON format
+     */
+    public static String toJson(Object object) {
+        return new Gson().toJson(object);
+    }
+
+    /**
+     * Author: <b>Alexander Young</b><p>
+     * Date: 4.9.17
+     * <p>
+     * spark java response transformer override that allows a route to return
+     * JSON<p>
+     *
+     * @return the spark response in JSON form
+     */
+    public static ResponseTransformer json() {
+        return JsonUtil::toJson;
+    }
 }
