9e4ba7fde13f8c252e38d8aaec9c9a0a96df611c
vishalpradhan <s3530077@student.rmit.edu.au>
2017-09-19 22:37:32 +1000

    search by price and distance
    filter the search function depending on the range selected by the user

--- src/main/resources/public/advancesearch.js
+++ src/main/resources/public/advancesearch.js
@@ -3,31 +3,86 @@
 * Date: 20-08-2017
 * First implemented on the date mentioned above and since then a lot of changes occured with this script
  */
-
-/*
- * Edited: 07-09-2017
- * this function gets data from a Json file at the front end and loads up the map 
- * as per the users selection..
- * this is a dynamic dependent drop down list of items where the second dropdown lists
- * depends on the selection of the first list by the user
- */
 $(document).ready(function () {
     /*
      * Edited: Vishal Pradhan
      * date: 16-09-2017
      * added slider bars for distance and price
-     */
-        $('#price_min').change(function(){  
-           var price = $(this).val();  
-           $("#price_range").text("Cars under Price $" + price);  
-             
-      });  
-        $('#distance_min').change(function(){  
-           var distance = $(this).val();  
-           $("#distance_range").text("Cars within the Distance of " + distance + " Km");  
-             
-      });  
+     */ 
+//        $('#price_min').change(function(){  
+//           var price = $(this).val();  
+//           $("#price_range").text("Cars under Price $" + price);  
+//           $.ajax({  
+//                url:"loadNearbyCars();",  
+//                method:"POST",  
+//                data:{price:price},  
+//                success:function(data){  
+//                     $("#price_range").fadeIn(500).html(data);  
+//                }  
+//           }); 
+//             
+//      });  
+//        $('#distance_min').change(function(){  
+//           var distance = $(this).val();  
+//           $("#distance_range").text("Cars within the Distance of " + distance + " Km");  
+//           $.ajax({  
+////                 $loadNearbyCars();
+//           }); 
+//             
+//      }); 
+
+        /*
+       * Edited: Vishal Pradhan
+       * Date: 19-09-2017
+       * New function to display the range based on the selected parameter by the user
+       */
+        var slider1 = document.getElementById("price_min");
+        var output1 = document.getElementById("price_range");
+        if(slider1.value === "0"){
+            output1.innerHTML= "Any";
+        }
+        else 
+        {
+            output1.innerHTML = slider1.value;
+        }
       
+        slider1.oninput = function() {
+        if(slider1.value === "0"){
+            output1.innerHTML= "Any";
+        }
+        else
+        {
+        output1.innerHTML = this.value;
+        }
+        };
+        
+        var slider = document.getElementById("distance_min");
+        var output = document.getElementById("distance_range");
+        if(slider.value === "0"){
+            output.innerHTML= "Any";
+        }
+        else 
+        {
+            output.innerHTML = slider.value;
+        }
+
+        slider.oninput = function() {
+            if(slider.value === "0"){
+            output.innerHTML= "Any";
+        }
+        else
+        {
+        output.innerHTML = this.value;
+        }    
+        };
+      
+      /*
+       * Edited: 07-09-2017
+       * this function gets data from a Json file at the front end and loads up the map 
+       * as per the users selection..
+       * this is a dynamic dependent drop down list of items where the second dropdown lists
+       * depends on the selection of the first list by the user
+       */
     load_json_data();
     function load_json_data() {
         var typeOption = '';
@@ -39,7 +94,7 @@ $(document).ready(function () {
          *values of the different types of cars available
          */
         $.getJSON("/get_all_types", function (data) {
-            typeOption = '<option value="empty">Select Type</option>';
+            typeOption = '<option value="empty">Any Type</option>';
             $.each(data, function (i, car_type) {
                 typeOption += "<option value='" + car_type + "'>" + car_type + "</option>";
             });
@@ -52,7 +107,7 @@ $(document).ready(function () {
          *just the make as well without selecting the type
          */
         $.getJSON("/get_all_makes", function (data) {
-            makeOption = '<option value="empty">Select Make</option>';
+            makeOption = '<option value="empty">Any Make</option>';
             $.each(data, function (i, car_make) {
                 makeOption += "<option value='" + car_make + "'>" + car_make + "</option>";
             });
@@ -89,7 +144,7 @@ $(document).ready(function () {
             {
                 if ($(this).val() !== '') {
                     $.getJSON("/get_models_by_make", {make: $(this).val()}, function (data) {
-                        modelOption = '<option value="empty">Select Model</option>';
+                        modelOption = '<option value="empty">Any Model</option>';
                         $.each(data, function (model, car_model) {
                             modelOption += "<option value='" + car_model + "'>" + car_model + "</option>";
                         });
@@ -101,12 +156,29 @@ $(document).ready(function () {
         });
     }
     ;
+    /*
+     * Edited: 19-09-2017 by Vishal Pradhan
+     * added distance and price variable to the search function.
+     * when the user clicks the search button, cars based on the selection of the user are displayed
+     */
     document.getElementById("search-btn").onclick = function () {
         $.getJSON("/search_for_cars", {make: $('#make').val(), model: $('#model').val(), type: $('#type').val()}, function (searchResults) {
             if (searchResults !== null) {
                 unloadAllCars();
                 $.each(searchResults, function (i, car) {
-                    loadSearchResults(searchResults);
+                    var distance = $('#distance_min').val();
+                    var maxCost = $('#price_min').val();
+                    if(distance === "0")
+                    {
+                        distance = 100;
+                    }
+                    if(maxCost === "0")
+                    {
+                        maxCost = 100000;
+                    }
+                    loadSearchResults(searchResults, distance, maxCost);
+                   
+                    
                 });
             }
         });
