56aa951268ceb131211dd8a37ceb8806a601cf7f
Alextg1 <s3330467@student.rmit.edu.au>
2017-10-10 19:59:15 +1100

    javadoc for CarSimulator

--- src/main/java/CarSimulator.java
+++ src/main/java/CarSimulator.java
@@ -3,6 +3,9 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
+ * Date: 20.9.17
+ * <p>
+ * contains methods that simulate the movement of a car on the map by randomly updating its latitude and longitude
  *
  * @author Alexander Young
  */
@@ -33,23 +36,41 @@ public class CarSimulator {
         newLat = car.getLat();
         newLng = car.getLng();
     }
-
+    
+    /**
+     * Author: <b>Alexander Young</b><p>
+     * Date: 20.9.17
+     * <p>
+     * sets the movementState flag to true and starts a timer that executes the moveCar() method every second<p>
+     */
     public void startMoving() {
         movementState = true;
         timer.schedule(new moveCar(), 0, 1000);
     }
-
+    
+    /**
+     * Author: <b>Alexander Young</b><p>
+     * Date: 20.9.17
+     * <p>
+     * sets the movementState flag to false cancels the timer that executes the moveCar() method<p>
+     */
     public void stopMoving() {
         movementState = false;
         timer.cancel();
     }
-
+    
+    /**
+     * Author: <b>Alexander Young</b><p>
+     * Date: 20.9.17
+     * <p>
+     * applies scaling factors to random doubles in order to simulate random x and y axis movement<p>
+     */
     class moveCar extends TimerTask {
 
         public void run() {
             randResult = rand.nextDouble();
             if(randResult < 0.05) {
-                System.out.print("result was :" + randResult + " 20% chance triggered");
+                //System.out.print("result was :" + randResult + " 20% chance triggered");
                 latMovement = ((rand.nextDouble()-0.5)*0.0005);
                 lngMovement = ((rand.nextDouble()-0.5)*0.0005);
             }
@@ -58,7 +79,7 @@ public class CarSimulator {
             newLat += latMovement;
             newLng += lngMovement;
             car = CarService.getCarByPlate_no(car.getPlate_no());
-            System.out.println("modifying car: " + car.getPlate_no() + " with new lat/lng: " + newLat + " " + newLng);
+            //System.out.println("modifying car: " + car.getPlate_no() + " with new lat/lng: " + newLat + " " + newLng);
             CarService.updateCarLatLng(car.getPlate_no(), newLat,  newLng);
         }
     }
